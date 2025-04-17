2b16415958fa963670afab35694751729f496cee
YogliB <yogev.boaron-ben-har@skai.io>
2025-04-18 01:08:19 +0300

    feat: enhance service worker activation and message handling

--- src/webllm-sw.ts
+++ src/webllm-sw.ts
@@ -1,5 +1,15 @@
 import * as webllm from '@mlc-ai/web-llm';
 
-self.addEventListener('activate', () => {
+self.addEventListener('activate', (event) => {
+	// @ts-expect-error - ExtendableEvent is not defined in the global scope
+	event.waitUntil(self.clients.claim());
+
 	new webllm.ServiceWorkerMLCEngineHandler();
+});
+
+self.addEventListener('message', (event) => {
+	if (event.data === 'claim') {
+		// @ts-expect-error - ExtendableEvent is not defined in the global scope
+		event.waitUntil(self.clients.claim());
+	}
 });
