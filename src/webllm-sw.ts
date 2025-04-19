e90e628f1c736462787c964daf97f2aa268c0250
YogliB <yogev.boaron-ben-har@skai.io>
2025-04-19 09:01:48 +0300

    refactor: remove initialization comments and unused variable for ServiceWorkerMLCEngineHandler

--- src/webllm-sw.ts
+++ src/webllm-sw.ts
@@ -1,11 +1,6 @@
 import * as webllm from '@mlc-ai/web-llm';
 
-// Initialize the ServiceWorkerMLCEngineHandler during the initial evaluation
-// This ensures the message event handler is registered properly
-// We need to instantiate this during initial script evaluation to avoid the warning:
-// "Event handler of 'message' event must be added on the initial evaluation of worker script"
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const handler = new webllm.ServiceWorkerMLCEngineHandler();
+new webllm.ServiceWorkerMLCEngineHandler();
 
 self.addEventListener('activate', (event) => {
 	// @ts-expect-error - ExtendableEvent is not defined in the global scope
