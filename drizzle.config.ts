e64981072064cd103ecba7b11d907257111f67a2
YogliB <yogev.boaron-ben-har@skai.io>
2025-04-21 11:08:24 +0300

    feat: update database URL configuration to remove default value and rely solely on environment variable

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,6 @@ export default {
 	out: './src/db/migrations',
 	dialect: 'postgresql',
 	dbCredentials: {
-		url:
-			process.env.DATABASE_URL ||
-			'postgres://postgres:postgres@localhost:5433/devbrain',
+		url: process.env.DATABASE_URL,
 	},
-} satisfies Config;
+} as Config;
